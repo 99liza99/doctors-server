0c8678ab81c2703c5455d8e99512c0e7b9fd532b
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-04 09:15:38 +0300

    add  new routes

--- app.js
+++ app.js
@@ -21,4 +21,5 @@ async function startApp() {
     console.log(e);
   }
 }
+
 startApp();
--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -14,10 +14,24 @@ router.get("/:id", async (req, res) => {
   res.status(200).json(doctor);
 });
 
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    res.status(400).json('Id not foun')
+  }
+  await doctorModel.findById(id).deleteOne();
+  res.status(200).json('Doctor was deleted');
+});
+
 router.post("/", async (req, res) => {
-  console.log(req.body);
   const { name, description, specialization, age } = req.body;
   const newDoctor = await doctorModel.create({name,description,specialization,age,});
+  res.status(200).json(newDoctor);
+});
+
+router.put("/", async (req, res) => {
+  const { name, description, specialization, age, id } = req.body;
+  const newDoctor = await doctorModel.updateOne(id, {name,description,specialization,age}, {new: true});
   res.status(200).json(newDoctor);
 });
 
