1792bdf18be14a2e0f3d7fd79c3d6f085d45b446
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-04 20:55:27 +0300

    fix code

--- app/routes/doctorRoute.js
+++ app/routes/doctorRoute.js
@@ -1,35 +1,35 @@
 const router = require("express").Router();
 const doctorService = require("../services/doctorsSrvice");
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   return await doctorService
     .getAll()
     .then((data) => res.status(200).json(data))
     .catch((err) => next(err));
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
   return await doctorService
     .getById(req.params.id)
     .then((data) => res.status(200).json(data))
     .catch((err) => next(err));
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res, next) => {
   return await doctorService
     .deleteById(req.params.id)
     .then((data) => res.status(200).json(data))
     .catch((err) => next(err));
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
   return await doctorService
     .create(req.body)
     .then((data) => res.status(200).json(data))
     .catch((err) => next(err));
 });
 
-router.put("/", async (req, res) => {
+router.put("/", async (req, res, next) => {
   return await doctorService
     .update(req.body)
     .then((data) => res.status(200).json(data))
