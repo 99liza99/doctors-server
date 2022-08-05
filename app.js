2a2777b55401afc449b29ed1c2e520c8b264029c
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-05 11:50:46 +0300

    fix appoitment service

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const doctorRoutes = require("./app/routes/doctorRoute");
-const doctorRoutes = require("./app/routes/appoitmentRoute");
+const appoitmentsRoutes = require("./app/routes/appoitmentRoute");
 
 
 
