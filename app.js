334f9682fadcf0705a6e87e660d9f0edeab4c759
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-07 13:11:28 +0300

    fix cors

--- app.js
+++ app.js
@@ -12,12 +12,9 @@ const PORT = 3000;
 
 // CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if(req.method === 'OPTIONS') {
-    res.header('Access-Control-Allows-Methods', 'PUT, POST, PATCH, DELETE, GET')
-    return res.status(200).json({});
-  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, PUT, POST, DELETE, OPTIONS");
   next();
 })
 
