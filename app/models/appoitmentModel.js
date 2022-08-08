a6a6252d7c0df67bb8b76357fa8dbd277952e052
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-08 09:57:04 +0300

    commit

--- app/models/appoitmentModel.js
+++ app/models/appoitmentModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const Appoitment = mongoose.Schema({
   name: { type: String, required: true },
-  _id: { type: String, required: true }
-  
+  doctor: { type: String, required: true }
+  /** add other fields */
 });
 
 module.exports = mongoose.model("Appoitment", Appoitment);
