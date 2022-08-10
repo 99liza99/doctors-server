6a4b50f706022122e8bf5889c2950793a2bce9e1
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-10 20:20:51 +0300

    add dotcotr to appoitment response

--- app/routes/appoitmentRoute.js
+++ app/routes/appoitmentRoute.js
@@ -4,9 +4,13 @@ const doctorsService = require("../services/doctorsSrvice");
 
 const _mapDoctorsToAppointments = (appointments, doctors) => {
   return appointments.map((a) => ({
-    ...a,
+    ...a.toObject(),
     doctor: doctors.find((d) => d._id.equals(a.doctor_id)),
   }));
+};
+
+const _mapDoctorToAppointment = (appointment, doctor) => {
+  return ({...appointment.toObject(), doctor:doctor});
 };
 
 router.get("/", async (req, res, next) => {
@@ -20,11 +24,10 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get("/:id", async (req, res, next) => {
-  return await appoitmentService
-
-    .getById(req.params.id)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => next(err));
+  const appoitemnt = await appoitmentService.getById(req.params.id);//get appoitemnt by id
+  const docotr = await doctorsService.getById(appoitemnt.doctor_id);//get doctor by doctp id
+  const newAppoitment = _mapDoctorToAppointment(appoitemnt, docotr);//map appotiemnt
+  return res.status(200).json(newAppoitment);
 });
 
 router.delete("/:id", async (req, res, next) => {
