6bf6d82aacf6c2deb5da84b53ea9b19bff1fb391
99liza99 <liza.owchinnikowa@gmail.com>
2022-08-10 20:36:13 +0300

    fix all routes

--- app/routes/appoitmentRoute.js
+++ app/routes/appoitmentRoute.js
@@ -13,6 +13,11 @@ const _mapDoctorToAppointment = (appointment, doctor) => {
   return ({...appointment.toObject(), doctor:doctor});
 };
 
+const returnAppoitementWithDoctor = async (appoitemnt) =>{
+  const docotr = await doctorsService.getById(appoitemnt.doctor_id);//get doctor by doctp id
+  return _mapDoctorToAppointment(appoitemnt, docotr);//map appotiemnt
+}
+
 router.get("/", async (req, res, next) => {
   const appointments = await appoitmentService.getAll();/** Get list all appoitments */
   appointmentDoctorsIds = appointments.map(
@@ -25,30 +30,22 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:id", async (req, res, next) => {
   const appoitemnt = await appoitmentService.getById(req.params.id);//get appoitemnt by id
-  const docotr = await doctorsService.getById(appoitemnt.doctor_id);//get doctor by doctp id
-  const newAppoitment = _mapDoctorToAppointment(appoitemnt, docotr);//map appotiemnt
-  return res.status(200).json(newAppoitment);
+  return res.status(200).json(await returnAppoitementWithDoctor(appoitemnt));
 });
 
 router.delete("/:id", async (req, res, next) => {
-  return await appoitmentService
-    .deleteById(req.params.id)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => next(err));
+  const appoitemnt =  await appoitmentService.deleteById(req.params.id)
+  return res.status(200).json(await returnAppoitementWithDoctor(appoitemnt));
 });
 
 router.post("/", async (req, res, next) => {
-  return await appoitmentService
-    .create(req.body)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => next(err));
+  const appoitemnt = await appoitmentService.create(req.body)
+  return res.status(200).json(await returnAppoitementWithDoctor(appoitemnt));
 });
 
 router.put("/", async (req, res, next) => {
-  return await appoitmentService
-    .update(req.body)
-    .then((data) => res.status(200).json(data))
-    .catch((err) => next(err));
+  const appoitemnt = await appoitmentService.update(req.body)
+  return res.status(200).json(await returnAppoitementWithDoctor(appoitemnt));
 });
 
 
